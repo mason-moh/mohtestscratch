3c3f49910fe6c7a05a48864af48329d3a35da059
mason <mason.mohkami@pushinteractions.com>
2015-11-30 15:00:39 -0600

    uploading stuff

---
+++ app/src/main/java/com/pi/jcenterfromscratch/MOHTest1.java
@@ -0,0 +1,11 @@
+package com.pi.jcenterfromscratch;
+
+/**
+ * Created by Mason on 30/11/2015.
+ */
+public class MOHTest1 {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
